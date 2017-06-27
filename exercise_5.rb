6e9e505147c80c08563e140b69a7be5ec983ce94
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-27 14:21:16 -0400

    Created_greet_backwards_program_for_Bob_Shirly_Sue_Andy

---
+++ exercise_5.rb
@@ -0,0 +1,8 @@
+def greet_backwards(name)
+  name.reverse!
+end
+
+puts "Hello, #{"Bob".reverse}! Welcome home."
+puts "Hello, #{"Shirly".reverse}! Welcome home."
+puts "Hello, #{"Sue".reverse}! Welcome home."
+puts "Hello, #{"Andy".reverse}! Welcome home."
