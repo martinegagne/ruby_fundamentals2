2793aef0c5908ac3514c94d995c3f29ed96cfffa
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-27 14:24:40 -0400

    added_display_name_twice_feature

--- exercise_5.rb
+++ exercise_5.rb
@@ -2,7 +2,7 @@ def greet_backwards(name)
   name.reverse!
 end
 
-puts "Hello, #{"Bob".reverse}! Welcome home."
-puts "Hello, #{"Shirly".reverse}! Welcome home."
-puts "Hello, #{"Sue".reverse}! Welcome home."
-puts "Hello, #{"Andy".reverse}! Welcome home."
+puts "Hello, #{"Bob".reverse * 2}! Welcome home."
+puts "Hello, #{"Shirly".reverse * 2}! Welcome home."
+puts "Hello, #{"Sue".reverse * 2}! Welcome home."
+puts "Hello, #{"Andy".reverse * 2}! Welcome home."
